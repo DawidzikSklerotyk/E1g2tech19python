b3b5ced38328ef6ec45e78bb2ccc647e4d2d30bd
XxDREFxX <114558712+DawidzikSklerotyk@users.noreply.github.com>
2022-10-26 07:43:24 +0200

    Create kartapracy3B.py

---
+++ kartapracy3B.py
@@ -0,0 +1,48 @@
+# # zad 1
+# for i in range(1, 31):
+#   print(i, end=" ")
+# # zad 2
+# for i in range(1, 100, 2):
+#   print(i**2, end=" ")
+# # zad 3
+# for i in range(1000, 10001):
+#   if i % 379 == 0:
+#     print(i)
+# # zad 4
+# for i in range(100, 1000):
+#   if i % 11 == 0:
+#     print(i)
+# # zad 5
+# suma = 0
+# ile = int(input("ile liczb: "))
+# for i in range(0, ile):
+#   n = int(input("podaj liczbę: "))
+#   suma = suma + n
+#   if i == ile - 1:
+#    print(suma)
+# # zad 6
+# n = int(input("liczba: "))
+
+  
+  
+# # zad 7
+
+# # zad 8
+# n = int(input("kwota wejściowa: "))
+# lat = int(input("ile lat: "))
+# miesiace = lat/12
+# for i in range(lat, lat + 1):
+#   print(n + n*((6*i)/100))
+# # zad 9
+# n = int(input("liczba: "))
+# n = n * 100
+# for i in range(21, n, 100):
+#   print(i)
+# # zad 10
+# import math
+# a = 0
+# for i in range(1, 1000):
+#   if i % 10 == 0: 
+#     a = i
+#   elif i - a == math.sqrt(i):
+#     print(i)
